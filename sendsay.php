24c522af668ba5d77ff0aa12d093bf377ddaf9ac
Alex <me@alexmil.ru>
2014-06-02 01:05:23 +0400

    1.1
    + Добавлен метод sequence_stats

--- sendsay.php
+++ sendsay.php
@@ -695,6 +695,25 @@ class Sendsay
 	}
 
 	/**
+	 * Запрашивает статистику последовательности.
+	 * 
+	 * @link  [https://pro.subscribe.ru/API/API.html#%D0%A1%D1%82%D0%B0%D1%82%D0%B8%D1%81%D1%82%D0%B8%D0%BA%D0%B0-%D0%BF%D0%BE%D1%81%D0%BB%D0%B5%D0%B4%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D0%B8][Документация]
+	 * 
+	 * @param  int  код последовательности
+	 * 
+	 * @return array
+	 */
+	public function sequence_stats($id)
+	{
+		$this->params = $this->auth+array(
+			'action' => 'sequence.stats',
+			'id'     => $id
+		);
+
+		return $this->send();
+	}
+
+	/**
 	 * Завершает прохождение подписчиками последовательности.
 	 * 
 	 * @link  [https://pro.subscribe.ru/API/API.html#%D0%9F%D1%80%D0%B5%D1%80%D0%B2%D0%B0%D1%82%D1%8C-%D0%BF%D1%80%D0%BE%D1%85%D0%BE%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BF%D0%BE%D1%81%D0%BB%D0%B5%D0%B4%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D0%B8][Документация]
