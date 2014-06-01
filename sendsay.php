d20eda907064d4a1f8ac4fc4e75144228070e08a
Alex <me@alexmil.ru>
2014-06-01 21:01:27 +0400

    1.1
    + Добавлен метод authext_ga_props

--- sendsay.php
+++ sendsay.php
@@ -964,6 +964,25 @@ class Sendsay
 	}
 
 	/**
+	 * Возвращает информацию об авторизации в Google Analytics.
+	 * 
+	 * @link  [https://pro.subscribe.ru/API/API.html#%D0%98%D0%BD%D1%84%D0%BE%D1%80%D0%BC%D0%B0%D1%86%D0%B8%D1%8F-%D0%BE%D0%B1-%D0%B0%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D0%B8-%D0%B2-Google-Analitics][Документация]
+	 * 
+	 * @param  string  код внешней авторизации
+	 *
+	 * @return array
+	 */
+	public function authext_ga_props($id)
+	{
+		$this->params = $this->auth+array(
+			'action' => 'authext.ga.props',
+			'id'     => $id
+		);
+		
+		return $this->send();
+	}
+
+	/**
 	 * Форматирует JSON-строку для отладки.
 	 *
 	 * @param  string  исходная JSON-строка
