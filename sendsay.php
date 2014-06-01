e76c8a9e2eab304f5838dafae0c3d929a6eb46fc
Alex <me@alexmil.ru>
2014-06-02 01:24:45 +0400

    1.1
    + Обновлён код метода sequence_member_stop

--- sendsay.php
+++ sendsay.php
@@ -830,24 +830,24 @@ class Sendsay
 	 * @link  [https://pro.subscribe.ru/API/API.html#%D0%9F%D1%80%D0%B5%D1%80%D0%B2%D0%B0%D1%82%D1%8C-%D0%BF%D1%80%D0%BE%D1%85%D0%BE%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BF%D0%BE%D1%81%D0%BB%D0%B5%D0%B4%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D0%B8][Документация]
 	 * 
 	 * @param  int    код последовательности
-	 * @param  mixed  код группы (string) или список емэйлов (array)
+	 * @param  mixed  список емэйлов (array) или код группы (string)
 	 * 
 	 * @return array
 	 */
-	public function sequence_member_stop($id, $data)
+	public function sequence_member_stop($id, $users)
 	{
 		$this->params = $this->auth+array(
 			'action' => 'sequence.member.stop',
 			'id'     => $id
 		);
 
-		if (gettype($data) == 'string')
+		if (is_array($users))
 		{
-			$this->params['group'] = $data;
+			$this->param('list', $users);
 		}
-		elseif (gettype($data) == 'array')
+		else
 		{
-			$this->params['list'] = $data;
+			$this->param('group', $users);
 		}
 		
 		return $this->send();
