b673d62cba1c31091d884c6d435aa178dc42bae9
Alex <me@alexmil.ru>
2014-06-02 00:00:22 +0400

    1.1
    + Добавлен метод sequence_set

--- sendsay.php
+++ sendsay.php
@@ -606,6 +606,36 @@ class Sendsay
 	}
 
 	/**
+	 * Изменяет параметры последовательности.
+	 * 
+	 * @link  [https://pro.subscribe.ru/API/API.html#%D0%98%D0%B7%D0%BC%D0%B5%D0%BD%D0%B8%D1%82%D1%8C-%D0%BF%D0%BE%D1%81%D0%BB%D0%B5%D0%B4%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D1%8C][Документация]
+	 * 
+	 * @param  int     код последовательности
+	 * @param  string  название последовательности
+	 * @param  bool    однократность последовательности
+	 * @param  bool    закрытость для новых участников
+	 * @param  bool    возобновлять прохождение при увеличении количества шагов
+	 * @param  bool    отстановка последовательности
+	 * 
+	 * @return array
+	 */
+	public function sequence_set($id, $name=NULL, $onlyonce=NULL, $closed=NULL, $rog=NULL, $pause=NULL)
+	{
+		$this->params = $this->auth+array(
+			'action' => 'sequence.set',
+			'id'     => $id
+		);
+
+		$this->param('name', $name);
+		$this->param('pause', $pause);
+		$this->param('closed', $closed);
+		$this->param('onlyonce', $onlyonce);
+		$this->param('resume_on_growing', $rog);
+		
+		return $this->send();
+	}
+
+	/**
 	 * Завершает прохождение подписчиками последовательности.
 	 * 
 	 * @link  [https://pro.subscribe.ru/API/API.html#%D0%9F%D1%80%D0%B5%D1%80%D0%B2%D0%B0%D1%82%D1%8C-%D0%BF%D1%80%D0%BE%D1%85%D0%BE%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BF%D0%BE%D1%81%D0%BB%D0%B5%D0%B4%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D0%B8][Документация]
