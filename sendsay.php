ac7bce3a1dd3f6f8a57d95a594fcb8bd688306f4
Alex <me@alexmil.ru>
2014-06-01 20:49:02 +0400

    1.1
    + Добавлен метод authext_delete

--- sendsay.php
+++ sendsay.php
@@ -945,6 +945,25 @@ class Sendsay
 	}
 
 	/**
+	 * Удаляет внешнюю авторизацию.
+	 * 
+	 * @link  [https://pro.subscribe.ru/API/API.html#%D0%A3%D0%B4%D0%B0%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5-%D0%B2%D0%BD%D0%B5%D1%88%D0%BD%D0%B5%D0%B9-%D0%B0%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D0%B8][Документация]
+	 * 
+	 * @param  string  код внешней авторизации
+	 *
+	 * @return array
+	 */
+	public function authext_delete($id)
+	{
+		$this->params = $this->auth+array(
+			'action' => 'authext.delete',
+			'id'     => $id
+		);
+		
+		return $this->send();
+	}
+
+	/**
 	 * Форматирует JSON-строку для отладки.
 	 *
 	 * @param  string  исходная JSON-строка
