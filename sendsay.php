8965a0e1ffd29cf83c12f7e5874fcb1ee1a1d414
Alex <me@alexmil.ru>
2014-06-01 20:32:01 +0400

    1.1
    + Добавлен метод authext_get

--- sendsay.php
+++ sendsay.php
@@ -862,6 +862,41 @@ class Sendsay
 	}
 
 	/**
+	 * Возвращает список внешних авторизаций.
+	 * 
+	 * @link  [https://pro.subscribe.ru/API/API.html#%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA-%D0%B2%D0%BD%D0%B5%D1%88%D0%BD%D0%B8%D1%85-%D0%B0%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D0%B9][Документация]
+	 *
+	 * @return array
+	 */
+	public function authext_list()
+	{
+		$this->params = $this->auth+array(
+			'action' => 'authext.list'
+		);
+		
+		return $this->send();
+	}
+
+	/**
+	 * Считывает параметры внешней авторизации.
+	 * 
+	 * @link  [https://pro.subscribe.ru/API/API.html#%D0%A7%D1%82%D0%B5%D0%BD%D0%B8%D0%B5-%D0%B2%D0%BD%D0%B5%D1%88%D0%BD%D0%B5%D0%B9-%D0%B0%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D0%B8][Документация]
+	 * 
+	 * @param  string  код внешней авторизации
+	 *
+	 * @return array
+	 */
+	public function authext_get($id)
+	{
+		$this->params = $this->auth+array(
+			'action' => 'authext.get',
+			'id'     => $id
+		);
+		
+		return $this->send();
+	}
+
+	/**
 	 * Форматирует JSON-строку для отладки.
 	 *
 	 * @param  string  исходная JSON-строка
