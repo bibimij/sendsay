bf98a9e6740a8421749cc1c1fd28d248da73d9d8
Alex <me@alexmil.ru>
2014-06-01 21:27:03 +0400

    1.1
    + Добавлен метод sequence_list

--- sendsay.php
+++ sendsay.php
@@ -543,6 +543,22 @@ class Sendsay
 	}
 
 	/**
+	 * Возвращает список последовательностей.
+	 * 
+	 * @link  [https://pro.subscribe.ru/API/API.html#%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA-%D0%BF%D0%BE%D1%81%D0%BB%D0%B5%D0%B4%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BEc%D1%82%D0%B5%D0%B9][Документация]
+	 * 
+	 * @return array
+	 */
+	public function sequence_list()
+	{
+		$this->params = $this->auth+array(
+			'action' => 'sequence.list'
+		);
+		
+		return $this->send();
+	}
+
+	/**
 	 * Завершает прохождение подписчиками последовательности.
 	 * 
 	 * @link  [https://pro.subscribe.ru/API/API.html#%D0%9F%D1%80%D0%B5%D1%80%D0%B2%D0%B0%D1%82%D1%8C-%D0%BF%D1%80%D0%BE%D1%85%D0%BE%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BF%D0%BE%D1%81%D0%BB%D0%B5%D0%B4%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D0%B8][Документация]
@@ -555,8 +571,8 @@ class Sendsay
 	public function sequence_member_stop($id, $data)
 	{
 		$this->params = $this->auth+array(
-			'action'       => 'sequence.member.stop',
-			'id'           => $id
+			'action' => 'sequence.member.stop',
+			'id'     => $id
 		);
 
 		if (gettype($data) == 'string')
