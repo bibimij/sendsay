2d4a49332674c4181b74ee9f63788db263cd0f04
Alex <me@alexmil.ru>
2014-06-02 01:13:37 +0400

    1.1
    + Добавлен метод sequence_member_list

--- sendsay.php
+++ sendsay.php
@@ -714,6 +714,30 @@ class Sendsay
 	}
 
 	/**
+	 * Возвращает список участников последовательности.
+	 * 
+	 * @link  [https://pro.subscribe.ru/API/API.html#%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA-%D1%83%D1%87%D0%B0%D1%81%D1%82%D0%BD%D0%B8%D0%BA%D0%BE%D0%B2-%D0%BF%D0%BE%D1%81%D0%BB%D0%B5%D0%B4%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D0%B8][Документация]
+	 * 
+	 * @param  int     код последовательности
+	 * @param  string  способ группировки (member|step) или не группировать (NULL)
+	 * @param  array   список интересующих шагов
+	 * 
+	 * @return array
+	 */
+	public function sequence_member_list($id, $group=NULL, $steps=NULL)
+	{
+		$this->params = $this->auth+array(
+			'action' => 'sequence.member.list',
+			'id'     => $id
+		);
+
+		$this->param('groupby', $group);
+		$this->param('steps', $steps);
+
+		return $this->send();
+	}
+
+	/**
 	 * Завершает прохождение подписчиками последовательности.
 	 * 
 	 * @link  [https://pro.subscribe.ru/API/API.html#%D0%9F%D1%80%D0%B5%D1%80%D0%B2%D0%B0%D1%82%D1%8C-%D0%BF%D1%80%D0%BE%D1%85%D0%BE%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BF%D0%BE%D1%81%D0%BB%D0%B5%D0%B4%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D0%B8][Документация]
