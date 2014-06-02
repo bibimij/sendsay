1f040021bda8b31534f824de1a50f14f6adcd536
Alex <me@alexmil.ru>
2014-06-02 14:05:07 +0400

    1.1
    + Добавлен метод anketa_create

--- sendsay.php
+++ sendsay.php
@@ -168,7 +168,31 @@ class Sendsay
 		);
 		
 		return $this->send();
-	}	
+	}
+	
+	/**
+	 * Создаёт анкету.
+	 * 
+	 * @link  [https://pro.subscribe.ru/API/API.html#%D0%A1%D0%BE%D0%B7%D0%B4%D0%B0%D0%BD%D0%B8%D0%B5-%D0%B0%D0%BD%D0%BA%D0%B5%D1%82%D1%8B][Документация]
+	 * 
+	 * @param  string  название анкеты
+	 * @param  string  код анкеты
+	 * @param  string  код копируемой анкеты
+	 * 
+	 * @return array
+	 */
+	public function anketa_create($name, $id=NULL, $copy=NULL)
+	{
+		$this->params = $this->auth+array(
+			'action' => 'anketa.create',
+			'name'   => $name
+		);
+
+		$this->param('id', $id);
+		$this->param('copy_from', $copy);
+		
+		return $this->send();
+	}
 	
 	/**
 	 * Проверяет список адресов на синтаксическую верность, доступность и возвращает нормализованый вариант написания.
