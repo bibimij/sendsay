eb6c09323bf30f375df63bfc97da407c07f459e8
Alex <me@alexmil.ru>
2014-06-17 11:10:58 +0400

    1.2
    + Добавлен метод anketa_quest_set

--- sendsay.php
+++ sendsay.php
@@ -237,6 +237,27 @@ class Sendsay
 	}
 	
 	/**
+	 * Изменяет вопросы анкеты.
+	 * 
+	 * @link  [https://pro.subscribe.ru/API/API.html#%D0%98%D0%B7%D0%BC%D0%B5%D0%BD%D0%B5%D0%BD%D0%B8%D0%B5-%D0%B2%D0%BE%D0%BF%D1%80%D0%BE%D1%81%D0%B0-%D0%B0%D0%BD%D0%BA%D0%B5%D1%82%D1%8B][Документация]
+	 * 
+	 * @param  string  код анкеты
+	 * @param  array   один или несколько вопросов анкеты
+	 * 
+	 * @return array
+	 */
+	public function anketa_quest_set($anketa, $questions)
+	{
+		$this->params = $this->auth+array(
+			'action'    => 'anketa.quest.set',
+			'anketa.id' => $anketa,
+			'obj'       => $questions
+		);
+		
+		return $this->send();
+	}
+	
+	/**
 	 * Удаляет вопрос из анкеты.
 	 * 
 	 * @link  [https://pro.subscribe.ru/API/API.html#%D0%A3%D0%B4%D0%B0%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5-%D0%B2%D0%BE%D0%BF%D1%80%D0%BE%D1%81%D0%B0-%D0%B0%D0%BD%D0%BA%D0%B5%D1%82%D1%8B][Документация]
