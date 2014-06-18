4165a05768d13e9f897929b8bfc6e47ba8ab5d1f
Alex <me@alexmil.ru>
2014-06-18 11:54:27 +0400

    1.2
    + Добавлен метод anketa_quest_response_order

--- sendsay.php
+++ sendsay.php
@@ -300,6 +300,29 @@ class Sendsay
 	}
 	
 	/**
+	 * Изменяет порядок ответов.
+	 * 
+	 * @link  [https://pro.subscribe.ru/API/API.html#%D0%98%D0%B7%D0%BC%D0%B5%D0%BD%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BF%D0%BE%D0%B7%D0%B8%D1%86%D0%B8%D0%B8-%D0%BE%D1%82%D0%B2%D0%B5%D1%82%D0%B0-%D0%B2%D0%BE%D0%BF%D1%80%D0%BE%D1%81%D0%B0][Документация]
+	 * 
+	 * @param  string  код анкеты
+	 * @param  string  код вопроса
+	 * @param  array   коды ответов в нужном порядке
+	 * 
+	 * @return array
+	 */
+	public function anketa_quest_response_order($anketa, $question, $order)
+	{
+		$this->params = $this->auth+array(
+			'action'    => 'anketa.quest.response.order',
+			'anketa.id' => $anketa,
+			'id'        => $question,
+			'order'     => $order
+		);
+		
+		return $this->send();
+	}
+	
+	/**
 	 * Проверяет список адресов на синтаксическую верность, доступность и возвращает нормализованый вариант написания.
 	 * 
 	 * @link  [https://pro.subscribe.ru/API/API.html#%D0%9F%D1%80%D0%BE%D0%B2%D0%B5%D1%80%D0%BA%D0%B0-%D0%B0%D0%B4%D1%80%D0%B5%D1%81%D0%BE%D0%B2][Документация]
