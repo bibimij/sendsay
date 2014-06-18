9268cd989016dabbb6474bf712e136daac564c94
Alex <me@alexmil.ru>
2014-06-19 00:45:50 +0400

    1.3
    + Добавлен метод format_list

--- sendsay.php
+++ sendsay.php
@@ -117,6 +117,22 @@ class Sendsay
 	}
 	
 	/**
+	 * Возвращает список форматов и шаблонов.
+	 * 
+	 * @link  [https://pro.subscribe.ru/API/API.html#%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA-%D1%84%D0%BE%D1%80%D0%BC%D0%B0%D1%82%D0%BE%D0%B2%D1%88%D0%B0%D0%B1%D0%BB%D0%BE%D0%BD%D0%BE%D0%B2][Документация]
+	 * 
+	 * @return array
+	 */
+	public function format_list()
+	{
+		$this->params = $this->auth+array(
+			'action' => 'format.list'
+		);
+		
+		return $this->send();
+	}
+	
+	/**
 	 * Возвращает список анкет.
 	 * 
 	 * @link  [https://pro.subscribe.ru/API/API.html#%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA-%D0%B0%D0%BD%D0%BA%D0%B5%D1%82][Документация]
