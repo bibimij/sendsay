8e79376594e814c075ed9baec56118f0ce5bac9b
Alex <me@alexmil.ru>
2014-06-20 01:27:31 +0400

    1.4
    + Добавлен метод group_snapshot

--- sendsay.php
+++ sendsay.php
@@ -816,6 +816,38 @@ class Sendsay
 	}
 	
 	/**
+	 * Создаёт копию подписчиков группы.
+	 * 
+	 * @link  [https://pro.subscribe.ru/API/API.html#%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA-%D0%B3%D1%80%D1%83%D0%BF%D0%BF%D1%8B-%D0%A0%D0%B0%D1%81%D1%88%D0%B8%D1%80%D0%B8%D1%82%D1%8C-%D0%B3%D1%80%D1%83%D0%BF%D0%BF%D1%83-%D1%81%D0%BF%D0%B8%D1%81%D0%BE%D0%BA][Документация]
+	 * 
+	 * @param  mixed   код группы (string) или список подписчиков (array)
+	 * @param  string  код группы
+	 * @param  bool    очистить группу перед внесением
+	 * @param  bool    асинхронность вызова
+	 * 
+	 * @return array
+	 */
+	public function group_snapshot($from, $to, $clean=FALSE, $sync=FALSE)
+	{
+		$this->params = $this->auth+array(
+			'action' => 'group.snapshot',
+			'to'     => array('id' => $to, 'clean' => $clean),
+			'from'   => array('sync' => $sync)
+		);
+
+		if (is_string($from))
+		{
+			$this->params['from']['group'] = $from;
+		}
+		elseif (is_array($from))
+		{
+			$this->params['from']['list'] = $from;
+		}
+		
+		return $this->send();
+	}
+	
+	/**
 	 * Возвращает общую статистику по группе.
 	 * 
 	 * @link  [https://pro.subscribe.ru/API/API.html#%D0%9E%D0%B1%D1%89%D0%B0%D1%8F-%D1%81%D1%82%D0%B0%D1%82%D0%B8%D1%81%D1%82%D0%B8%D0%BA%D0%B0-%D0%BF%D0%BE-%D0%B3%D1%80%D1%83%D0%BF%D0%BF%D0%B5][Документация]
