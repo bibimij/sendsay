db2acafe4a029602ce5697dd878ebce14d94322e
Alex <me@alexmil.ru>
2014-06-19 21:28:36 +0400

    1.4
    + Добавлен метод group_set

--- sendsay.php
+++ sendsay.php
@@ -774,6 +774,26 @@ class Sendsay
 	}
 	
 	/**
+	 * Изменяет название группы.
+	 * 
+	 * @link  [https://pro.subscribe.ru/API/API.html#%D0%98%D0%B7%D0%BC%D0%B5%D0%BD%D0%B8%D1%82%D1%8C-%D0%B3%D1%80%D1%83%D0%BF%D0%BF%D1%83][Документация]
+	 * 
+	 * @param  string  код группы
+	 * @param  string  название группы
+	 * 
+	 * @return array
+	 */
+	public function group_set($id, $name)
+	{
+		$this->params = $this->auth+array(
+			'action' => 'group.set',
+			'id'     => $id,
+			'name'   => $name
+		);
+		
+		return $this->send();
+	}
+	
 	 * Возвращает общую статистику по группе.
 	 * 
 	 * @link  [https://pro.subscribe.ru/API/API.html#%D0%9E%D0%B1%D1%89%D0%B0%D1%8F-%D1%81%D1%82%D0%B0%D1%82%D0%B8%D1%81%D1%82%D0%B8%D0%BA%D0%B0-%D0%BF%D0%BE-%D0%B3%D1%80%D1%83%D0%BF%D0%BF%D0%B5][Документация]
