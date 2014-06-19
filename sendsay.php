854af2b62f38d41c37c53763d5820c52f7821f14
Alex <me@alexmil.ru>
2014-06-19 11:10:33 +0400

    1.4
    + Добавлен метод group_clean

--- sendsay.php
+++ sendsay.php
@@ -739,6 +739,41 @@ class Sendsay
 	}
 	
 	/**
+	 * Удаляет участников группы-списка.
+	 * 
+	 * @link  [https://pro.subscribe.ru/API/API.html#%D0%9E%D1%87%D0%B8%D1%81%D1%82%D0%B8%D1%82%D1%8C-%D0%B3%D1%80%D1%83%D0%BF%D0%BF%D1%83-%D1%81%D0%BF%D0%B8%D1%81%D0%BE%D0%BA][Документация]
+	 * 
+	 * @param  string  код группы
+	 * @param  mixed   подписчики, которых надо удалить (all | string — емэйл подписчика | array — список емэйлов)
+	 * @param  bool    асинхронность запуска
+	 * 
+	 * @return array
+	 */
+	public function group_clean($id, $list='all', $sync=FALSE)
+	{
+		$this->params = $this->auth+array(
+			'action' => 'group.clean',
+			'id'     => $id,
+			'sync'   => $sync
+		);
+
+		if ($list === 'all')
+		{
+			$this->params['all'] = TRUE;
+		}
+		elseif (is_string($list))
+		{
+			$this->params['email'] = $list;
+		}
+		elseif (is_array($list))
+		{
+			$this->params['list'] = $list;
+		}
+		
+		return $this->send();
+	}
+	
+	/**
 	 * Возвращает общую статистику по группе.
 	 * 
 	 * @link  [https://pro.subscribe.ru/API/API.html#%D0%9E%D0%B1%D1%89%D0%B0%D1%8F-%D1%81%D1%82%D0%B0%D1%82%D0%B8%D1%81%D1%82%D0%B8%D0%BA%D0%B0-%D0%BF%D0%BE-%D0%B3%D1%80%D1%83%D0%BF%D0%BF%D0%B5][Документация]
