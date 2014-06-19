79691459587b9a938e353b1187d84b9f4d8a84ca
Alex <me@alexmil.ru>
2014-06-19 21:34:49 +0400

    1.4
    + Добавлен метод group_get

--- sendsay.php
+++ sendsay.php
@@ -794,6 +794,28 @@ class Sendsay
 		return $this->send();
 	}
 	
+	/**
+	 * Считывает группу.
+	 * 
+	 * @link  [https://pro.subscribe.ru/API/API.html#%D0%9F%D1%80%D0%BE%D1%87%D0%B8%D1%82%D0%B0%D1%82%D1%8C-%D0%B3%D1%80%D1%83%D0%BF%D0%BF%D1%83][Документация]
+	 * 
+	 * @param  mixed  код группы (string) или список групп (array)
+	 * @param  bool   возвращать фильтр группы
+	 * 
+	 * @return array
+	 */
+	public function group_get($id, $filter=FALSE)
+	{
+		$this->params = $this->auth+array(
+			'action'      => 'group.get',
+			'id'          => $id,
+			'with_filter' => $filter
+		);
+		
+		return $this->send();
+	}
+	
+	/**
 	 * Возвращает общую статистику по группе.
 	 * 
 	 * @link  [https://pro.subscribe.ru/API/API.html#%D0%9E%D0%B1%D1%89%D0%B0%D1%8F-%D1%81%D1%82%D0%B0%D1%82%D0%B8%D1%81%D1%82%D0%B8%D0%BA%D0%B0-%D0%BF%D0%BE-%D0%B3%D1%80%D1%83%D0%BF%D0%BF%D0%B5][Документация]
