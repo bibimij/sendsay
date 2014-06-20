92fe9b8c99c0a885e59bfbc6bd8131f99fd214b2
Alex <me@alexmil.ru>
2014-06-20 12:41:08 +0400

    1.4
    + Добавлен метод group_delete

--- sendsay.php
+++ sendsay.php
@@ -887,6 +887,26 @@ class Sendsay
 		return $this->send();
 	}
 	
+	/**
+	 * Удаляет группу.
+	 * 
+	 * @link  [https://pro.subscribe.ru/API/API.html#%D0%A3%D0%B4%D0%B0%D0%BB%D0%B8%D1%82%D1%8C-%D0%B3%D1%80%D1%83%D0%BF%D0%BF%D1%83][Документация]
+	 * 
+	 * @param  string  код группы
+	 * 
+	 * @return array
+	 */
+	public function group_delete($id)
+	{
+		$this->params = $this->auth+array(
+			'action' => 'group.delete',
+			'id'     => $id
+		);
+
+		return $this->send();
+	}
+	
+	/**
 	 * Возвращает общую статистику по группе.
 	 * 
 	 * @link  [https://pro.subscribe.ru/API/API.html#%D0%9E%D0%B1%D1%89%D0%B0%D1%8F-%D1%81%D1%82%D0%B0%D1%82%D0%B8%D1%81%D1%82%D0%B8%D0%BA%D0%B0-%D0%BF%D0%BE-%D0%B3%D1%80%D1%83%D0%BF%D0%BF%D0%B5][Документация]
