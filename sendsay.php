cba92a400198624d896e46afbc206f0a07cc5542
Alex <me@alexmil.ru>
2014-06-20 12:40:48 +0400

    1.4
    + Добавлен метод group_filter_set

--- sendsay.php
+++ sendsay.php
@@ -866,6 +866,27 @@ class Sendsay
 		return $this->send();
 	}
 	
+	/**
+	 * Изменяет правила фильтрации группы.
+	 * 
+	 * @link  [https://pro.subscribe.ru/API/API.html#%D0%A3%D1%81%D1%82%D0%B0%D0%BD%D0%BE%D0%B2%D0%B8%D1%82%D1%8C-%D0%BF%D1%80%D0%B0%D0%B2%D0%B8%D0%BB%D0%B0-%D1%84%D0%B8%D0%BB%D1%8C%D1%82%D1%80%D0%B0%D1%86%D0%B8%D0%B8-%D0%B3%D1%80%D1%83%D0%BF%D0%BF%D1%8B][Документация]
+	 * 
+	 * @param  string  код группы
+	 * @param  array   правила фильтрации
+	 * 
+	 * @return array
+	 */
+	public function group_filter_set($id, $filter)
+	{
+		$this->params = $this->auth+array(
+			'action' => 'group.filter.set',
+			'id'     => $id,
+			'filter' => $filter
+		);
+
+		return $this->send();
+	}
+	
 	 * Возвращает общую статистику по группе.
 	 * 
 	 * @link  [https://pro.subscribe.ru/API/API.html#%D0%9E%D0%B1%D1%89%D0%B0%D1%8F-%D1%81%D1%82%D0%B0%D1%82%D0%B8%D1%81%D1%82%D0%B8%D0%BA%D0%B0-%D0%BF%D0%BE-%D0%B3%D1%80%D1%83%D0%BF%D0%BF%D0%B5][Документация]
